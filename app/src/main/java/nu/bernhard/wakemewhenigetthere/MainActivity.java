7f75870a8e65c47dd4ce872940f099795618b4e0
Bernhard Hahn <bernhard.hahn@bernhard.nu>
2015-06-26 09:34:59 +0200

    Use AppConpatActivity
    Don't use ActionBarActivity since it is deprecated

--- app/src/main/java/nu/bernhard/wakemewhenigetthere/MainActivity.java
+++ app/src/main/java/nu/bernhard/wakemewhenigetthere/MainActivity.java
@@ -1,12 +1,12 @@
 package nu.bernhard.wakemewhenigetthere;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +17,6 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
