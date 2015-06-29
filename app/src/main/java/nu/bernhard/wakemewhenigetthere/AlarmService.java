ee2b1112a7304c914c9ecb66fa1dea807797c274
Bernhard Hahn <bernhard.hahn@bernhard.nu>
2015-06-29 13:42:21 +0200

    Move geofence interaction to AlarmService

--- app/src/main/java/nu/bernhard/wakemewhenigetthere/AlarmService.java
+++ app/src/main/java/nu/bernhard/wakemewhenigetthere/AlarmService.java
@@ -5,11 +5,28 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.Context;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
 
-public class AlarmService extends NonStoppingIntentService {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlarmService extends NonStoppingIntentService implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        ResultCallback<Status> {
+
     private static final String ACTION_FOREGROUND =
             "nu.bernhard.wakemewhenigetthere.action.FOREGROUND";
     private static final String ACTION_ENTER_GEOFENCE =
@@ -20,7 +37,9 @@ public class AlarmService extends NonStoppingIntentService {
     private static final String EXTRA_GEOFENCE_ID =
             "nu.bernhard.wakemewhenigetthere.extra.GEOFENCE_ID";
     private static final String TAG = "AlarmService";
-    private IBinder binder  = new AlarmServiceBinder();;
+    private IBinder binder  = new AlarmServiceBinder();
+    private Alarms alarms = new Alarms();
+    private GoogleApiClient googleApiClient;
 
     public static void setForeground(Context context, boolean foreground) {
         Intent intent = new Intent(context, AlarmService.class);
@@ -40,6 +59,27 @@ public class AlarmService extends NonStoppingIntentService {
         super(TAG);
     }
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initAlarms();
+        googleApiClient = new GoogleApiClient.Builder(getApplicationContext())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        googleApiClient.connect();
+    }
+
+
+    public Alarms getAlarms() {
+        return alarms;
+    }
+
+    public void setAlarms(Alarms alarms) {
+        this.alarms = alarms;
+    }
+
     public class AlarmServiceBinder extends Binder {
         AlarmService getService() {
             return AlarmService.this;
@@ -53,7 +93,9 @@ public class AlarmService extends NonStoppingIntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        Log.d(TAG, "onHandleIntent");
         if (intent != null) {
+            Log.d(TAG, "onHandleIntent: intent: " + intent.toString());
             final String action = intent.getAction();
 
             if (ACTION_FOREGROUND.equals(action)) {
@@ -64,6 +106,11 @@ public class AlarmService extends NonStoppingIntentService {
                 handleActionEnterGeofence(geofenceId);
             }
         }
+    }
+
+    public void addAlarm(Alarm alarm) {
+        alarms.add(alarm);
+        addGeofences();
     }
 
     private void handleActionEnterGeofence(String geofenceId) {
@@ -95,6 +142,111 @@ public class AlarmService extends NonStoppingIntentService {
         } else {
             stopForeground(true);
         }
+    }
+
+
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.i(TAG, "Connected to GoogleApiClient");
+        addGeofences();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason.
+        Log.i(TAG, "Connection suspended");
+
+        // onConnected() will be called again automatically when the service reconnects
+    }
+
+    private void addGeofences() {
+        if (!googleApiClient.isConnected()) {
+            //TODO: raise error
+            Log.d(TAG, "addGeofences: googleApiClient is not connected");
+            return;
+        }
+
+
+        try {
+            LocationServices.GeofencingApi.addGeofences(
+                    googleApiClient,
+                    getGeofencingRequest(),
+                    getGeofencePendingIntent()
+            ).setResultCallback(this); // Result processed in onResult().
+        } catch (SecurityException securityException) {
+            Log.e(TAG, "Invalid location permission. " +
+                    "You need to use ACCESS_FINE_LOCATION with geofences", securityException);
+        }
+    }
+
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+
+        // The INITIAL_TRIGGER_ENTER flag indicates that geofencing service should trigger a
+        // GEOFENCE_TRANSITION_ENTER notification when the geofence is added and if the device
+        // is already inside that geofence.
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofences(getLocations());
+        return builder.build();
+    }
+
+    private List<Geofence> getLocations() {
+        List<Geofence> locations = new ArrayList<>();
+        for (Alarm alarm : alarms.getAll()) {
+            locations.add(new Geofence.Builder()
+                    .setRequestId(alarm.getName())
+                    .setCircularRegion(alarm.getLat(), alarm.getLon(), alarm.getRadius())
+                    .setExpirationDuration(24 * 60 * 60 * 1000)
+                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                            Geofence.GEOFENCE_TRANSITION_EXIT)
+                    .build());
+        }
+        return locations;
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        Intent intent = new Intent(getApplicationContext(),
+                GeofenceTransitionsIntentService.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
+        // addGeofences() and removeGeofences().
+        return PendingIntent.getService(getApplicationContext(),
+                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    @Override
+    public void onResult(Status status) {
+        if (status.isSuccess()) {
+            Log.d(TAG, "Add geofence: Success");
+            Log.d(TAG, "status: " + status.getStatusMessage());
+        } else {
+            // Get the status code for the error and log it using a user-friendly message.
+            String errorMessage = GeofenceErrorMessages.getErrorString(
+                    getApplicationContext(),
+                    status.getStatusCode());
+            Log.e(TAG, errorMessage);
+        }
+    }
+
+    private void initAlarms() {
+        String alarmJson = "{name: \"Test Alarm\", lon: 15.566608, lat: 58.412103, radius: 250, active: true}";
+        String alarmJson2 = "{name: \"Test Alarm22\", lon: 15.566608, lat: 58.412103, radius: 500, active: true}";
+        String alarmJson3 = "{name: \"Test Alarm333\", lon: 15.566608, lat: 58.412103, radius: 1250, active: true}";
+
+        alarms = new Alarms();
+        Alarm alarm = new Alarm(alarmJson);
+        alarms.add(alarm);
+        alarm = new Alarm(alarmJson2);
+        alarms.add(alarm);
+        alarm = new Alarm(alarmJson3);
+        alarms.add(alarm);
     }
 
 }
--- app/src/main/java/nu/bernhard/wakemewhenigetthere/MainActivityFragment.java
+++ app/src/main/java/nu/bernhard/wakemewhenigetthere/MainActivityFragment.java
@@ -1,6 +1,5 @@
 package nu.bernhard.wakemewhenigetthere;
 
-import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -15,27 +14,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.Geofence;
-import com.google.android.gms.location.GeofencingRequest;
-import com.google.android.gms.location.LocationServices;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
-/**
- * A placeholder fragment containing a simple view.
- */
-public class MainActivityFragment extends Fragment implements
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        ResultCallback<Status> {
+public class MainActivityFragment extends Fragment {
 
     private static final String TAG = MainActivityFragment.class.getName();
 
@@ -44,8 +25,6 @@ public class MainActivityFragment extends Fragment implements
     private Button stopServiceForegroundButton;
     private ListView alarmsListView;
 
-    private Alarms alarms;
-    private GoogleApiClient googleApiClient;
     private EditText newAlarmRadiusInput;
     private EditText newAlarmNameInput;
     private EditText newAlarmLatInput;
@@ -64,6 +43,7 @@ public class MainActivityFragment extends Fragment implements
             alarmService = binder.getService();
             serviceBound = true;
             Log.d(TAG, "onServiceConnected: " + className.toString());
+            getAlarmsFromService();
         }
 
         @Override
@@ -74,6 +54,17 @@ public class MainActivityFragment extends Fragment implements
         }
     };
 
+    private void getAlarmsFromService() {
+        if ( serviceBound ) {
+            Alarms alarms = alarmService.getAlarms();
+            alarmsAdapter = new AlarmsAdapter(getActivity().getApplicationContext(), alarms);
+            alarmsListView.setAdapter(alarmsAdapter);
+        } else {
+            Log.d(TAG, "alarms service not bound: can't create alarm's adapter");
+        }
+
+    }
+
     public MainActivityFragment() {
     }
 
@@ -82,14 +73,12 @@ public class MainActivityFragment extends Fragment implements
         super.onStart();
         Intent intent = new Intent(getActivity(), AlarmService.class);
         getActivity().bindService(intent, alarmServiceConnection, Context.BIND_ADJUST_WITH_ACTIVITY);
-        googleApiClient.connect();
     }
 
     @Override
     public void onStop() {
         super.onStop();
         getActivity().unbindService(alarmServiceConnection);
-        googleApiClient.disconnect();
     }
 
     @Override
@@ -116,9 +105,11 @@ public class MainActivityFragment extends Fragment implements
                 Integer radius = Integer.parseInt((newAlarmRadiusInput.getText().toString()));
 
                 Alarm alarm = new Alarm(alarmName, lon, lat, radius, true);
-                alarms.add(alarm);
-                alarmsAdapter.notifyDataSetChanged();
-                addGeofences();
+
+                if(serviceBound) {
+                    alarmService.addAlarm(alarm);
+                    alarmsAdapter.notifyDataSetChanged();
+                }
 
             }
         });
@@ -137,121 +128,13 @@ public class MainActivityFragment extends Fragment implements
             }
         });
 
-        googleApiClient = new GoogleApiClient.Builder(getActivity().getApplicationContext())
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
 
-        initAlarms();
-        alarmsAdapter = new AlarmsAdapter(getActivity().getApplicationContext(), alarms);
-        alarmsListView.setAdapter(alarmsAdapter);
+
+
 
         return view;
     }
 
 
-    @Override
-    public void onConnected(Bundle connectionHint) {
-        Log.i(TAG, "Connected to GoogleApiClient");
-        addGeofences();
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
-        // onConnectionFailed.
-        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
-    }
-
-    @Override
-    public void onConnectionSuspended(int cause) {
-        // The connection to Google Play services was lost for some reason.
-        Log.i(TAG, "Connection suspended");
-
-        // onConnected() will be called again automatically when the service reconnects
-    }
-
-    public void addGeofences() {
-        if (!googleApiClient.isConnected()) {
-            Toast.makeText(getActivity().getApplicationContext(),
-                    getResources().getText(R.string.not_connected).toString(),
-                    Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        try {
-            LocationServices.GeofencingApi.addGeofences(
-                    googleApiClient,
-                    getGeofencingRequest(),
-                    getGeofencePendingIntent()
-            ).setResultCallback(this); // Result processed in onResult().
-        } catch (SecurityException securityException) {
-            Log.e(TAG, "Invalid location permission. " +
-                    "You need to use ACCESS_FINE_LOCATION with geofences", securityException);
-        }
-    }
-
-    private GeofencingRequest getGeofencingRequest() {
-        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
-
-        // The INITIAL_TRIGGER_ENTER flag indicates that geofencing service should trigger a
-        // GEOFENCE_TRANSITION_ENTER notification when the geofence is added and if the device
-        // is already inside that geofence.
-        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
-        builder.addGeofences(getLocations());
-        return builder.build();
-    }
-
-    private List<Geofence> getLocations() {
-        List<Geofence> locations = new ArrayList<>();
-        for (Alarm alarm : alarms.getAll()) {
-            locations.add(new Geofence.Builder()
-                    .setRequestId(alarm.getName())
-                    .setCircularRegion(alarm.getLat(), alarm.getLon(), alarm.getRadius())
-                    .setExpirationDuration(24 * 60 * 60 * 1000)
-                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
-                            Geofence.GEOFENCE_TRANSITION_EXIT)
-                    .build());
-        }
-        return locations;
-    }
-
-    private PendingIntent getGeofencePendingIntent() {
-        Intent intent = new Intent(getActivity().getApplicationContext(),
-                GeofenceTransitionsIntentService.class);
-        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
-        // addGeofences() and removeGeofences().
-        return PendingIntent.getService(getActivity().getApplicationContext(),
-                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-    }
-
-    @Override
-    public void onResult(Status status) {
-        if (status.isSuccess()) {
-            Log.d(TAG, "Add geofence: Success");
-            Log.d(TAG, "status: " + status.getStatusMessage());
-        } else {
-            // Get the status code for the error and log it using a user-friendly message.
-            String errorMessage = GeofenceErrorMessages.getErrorString(
-                    getActivity().getApplicationContext(),
-                    status.getStatusCode());
-            Log.e(TAG, errorMessage);
-        }
-    }
-
-    private void initAlarms() {
-        String alarmJson = "{name: \"Test Alarm\", lon: 15.566608, lat: 58.412103, radius: 250, active: true}";
-        String alarmJson2 = "{name: \"Test Alarm22\", lon: 15.566608, lat: 58.412103, radius: 500, active: true}";
-        String alarmJson3 = "{name: \"Test Alarm333\", lon: 15.566608, lat: 58.412103, radius: 1250, active: true}";
-
-        alarms = new Alarms();
-        Alarm alarm = new Alarm(alarmJson);
-        alarms.add(alarm);
-        alarm = new Alarm(alarmJson2);
-        alarms.add(alarm);
-        alarm = new Alarm(alarmJson3);
-        alarms.add(alarm);
-    }
 
 }
