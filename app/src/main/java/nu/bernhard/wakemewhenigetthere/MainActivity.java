018bddee1f0d109742ec2dd0735d6c9c3ea4c8f9
Bernhard Hahn <bernhard.hahn@bernhard.nu>
2015-06-29 14:27:06 +0200

    Remove settings menu

--- app/src/main/java/nu/bernhard/wakemewhenigetthere/MainActivity.java
+++ app/src/main/java/nu/bernhard/wakemewhenigetthere/MainActivity.java
@@ -2,8 +2,6 @@ package nu.bernhard.wakemewhenigetthere;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
     @Override
@@ -28,27 +24,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         AlarmService.setForeground(this, true);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 }
