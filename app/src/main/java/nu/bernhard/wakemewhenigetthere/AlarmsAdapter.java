d4eccb84dfcfa3aaf4d242d0b1b1bbf19d7e870f
Bernhard Hahn <bernhard.hahn@bernhard.nu>
2015-06-30 08:51:56 +0200

    Use Switch insted of ToggleButton in AlarmList

--- app/src/main/java/nu/bernhard/wakemewhenigetthere/AlarmsAdapter.java
+++ app/src/main/java/nu/bernhard/wakemewhenigetthere/AlarmsAdapter.java
@@ -6,8 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.ToggleButton;
 
 /**
  * Created by Bernhard on 2015-06-27.
@@ -47,7 +47,7 @@ public class AlarmsAdapter extends BaseAdapter {
         TextView title = (TextView)view.findViewById(R.id.alarm_list_item_title);
         TextView coords = (TextView)view.findViewById(R.id.alarm_list_item_coords);
         TextView radius = (TextView)view.findViewById(R.id.alarm_list_item_radius);
-        ToggleButton status = (ToggleButton)view.findViewById(R.id.alarm_list_item_active);
+        Switch status = (Switch)view.findViewById(R.id.alarm_list_item_active);
 
         final Alarm alarm = alarms.get(i);
         title.setText(alarm.getName());
