9d0f4f2296c163c897a19abf64e058e19b54060c
Bernhard Hahn <bernhard.hahn@bernhard.nu>
2015-07-11 14:14:06 +0200

    Comment Geofence Service

--- app/src/main/java/nu/bernhard/wakemewhenigetthere/GeofenceService.java
+++ app/src/main/java/nu/bernhard/wakemewhenigetthere/GeofenceService.java
@@ -11,44 +11,37 @@ import java.util.List;
 
 
 /**
- * Listener for geofence transition changes.
+ * GeofenceService receives geofence transition changes from
+ * Google Play Services geofencing api
  *
- * Receives geofence transition events from Location Services in the form of an Intent containing
- * the transition type and geofence id(s) that triggered the transition. Creates a notification
- * as the output.
+ * All enter transitions are forwarded to AlarmService
  */
 public class GeofenceService extends IntentService {
 
     private static final String TAG = GeofenceService.class.getName();
 
-    /**
-     * This constructor is required, and calls the super IntentService(String)
-     * constructor with the name for a worker thread.
-     */
     public GeofenceService() {
         super(TAG);
     }
 
     /**
-     * Handles incoming intents.
-     * @param intent sent by Location Services. This Intent is provided to Location
-     *               Services (inside a PendingIntent) when addGeofences() is called.
+     * Handle incomming messages from google play services geofencing api
+     *
+     * @param intent sent by geofencing api containing geofence transitions
      */
     @Override
     protected void onHandleIntent(Intent intent) {
         Log.d(TAG, "GeofenceTransitionsIntentService onHandleIntent");
         GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
         if (geofencingEvent.hasError()) {
-            String errorMessage = GeofenceErrorMessages.getErrorString(this,
-                    geofencingEvent.getErrorCode());
-            Log.e(TAG, errorMessage);
+            String errorMessage =
+                    GeofenceErrorMessages.getErrorString(this, geofencingEvent.getErrorCode());
+            Log.d(TAG, errorMessage);
             return;
         }
-
+        // Only forward Geofence enter transitions
         if (geofencingEvent.getGeofenceTransition() == Geofence.GEOFENCE_TRANSITION_ENTER) {
-
             List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
-
             for (Geofence geofence : triggeringGeofences) {
                 Log.d(TAG, "handle geofence with requestId: " + geofence.getRequestId());
                 AlarmService.enterGeofence(getApplicationContext(), geofence.getRequestId());
