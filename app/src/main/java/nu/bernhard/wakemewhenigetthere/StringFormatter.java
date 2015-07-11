85bf875fbc48f29befe35fb1d86269e1981430e8
Bernhard Hahn <bernhard.hahn@bernhard.nu>
2015-07-11 14:53:42 +0200

    Comment StringFormatter

--- app/src/main/java/nu/bernhard/wakemewhenigetthere/StringFormatter.java
+++ app/src/main/java/nu/bernhard/wakemewhenigetthere/StringFormatter.java
@@ -2,8 +2,18 @@ package nu.bernhard.wakemewhenigetthere;
 
 import android.content.Context;
 
+/**
+ * Static Helper Class to format string
+ */
 public class StringFormatter {
 
+    /**
+     * Format Radius string with correct unit
+     *
+     * @param context           ApplicationContext of string resource
+     * @param radiusInMetres    Radius in meter to be formatted as string
+     * @return                  A Formatted string of radiusInMetres with unit (m or km)
+     */
     public static String radiusStringFormatter(Context context, int radiusInMetres) {
         String unit = radiusInMetres >= 1000 ? "km" : "m";
         int radius = radiusInMetres >= 1000 ? radiusInMetres / 1000 : radiusInMetres;
